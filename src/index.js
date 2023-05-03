3d19b8de586195451ebc33b48ba15b4cb87aa5f1
Bisaso Ronald <bronnie313@gmail.com>
2023-05-03 19:53:41 +0300

    implement editing on the todo

--- src/index.js
+++ src/index.js
@@ -19,13 +19,13 @@ const populate = () => {
   items.innerHTML = '';
   todoList.forEach((item, i) => {
     items.innerHTML += ` 
-    <div class="list p-1 d-flex justify-content-between align-items-center mt-1">
-            <input type="checkbox">
-            <p class="m-0 space" contentEditable="true"> ${item.description} </P>
-            <i class="fa-solid fa-trash-can delete" data-index="${i}"></i>
-            <i class="fa-solid fa-ellipsis-vertical fa-lg view-more" data-index="${i}"></i>
-    </div>
-    `;
+        <div class="list p-1 d-flex justify-content-between align-items-center mt-1">
+                    <input type="checkbox">
+                    <p class="m-0 space" data-index="${i}" contentEditable="true"> ${item.description} </P>
+                    <i class="fa-solid fa-trash-can delete" data-index="${i}"></i>
+                    <i class="fa-solid fa-ellipsis-vertical fa-lg view-more" data-index="${i}"></i>
+        </div>
+        `;
     items.value = '';
 
     const viewMoreBtn = document.querySelectorAll('.view-more');
@@ -37,6 +37,27 @@ const populate = () => {
         const index = parseInt(e.target.dataset.index, 10);
         btn.style.display = 'none';
         deleteBtn[index].style.display = 'block';
+      });
+    });
+
+    const editContent = document.querySelectorAll('.space');
+
+    editContent.forEach((space) => {
+      space.addEventListener('blur', (e) => {
+        e.preventDefault();
+        const index = parseInt(e.target.dataset.index, 10);
+        todoList[index].description = space.innerText;
+        localStorage.setItem('todos', JSON.stringify(todoList));
+      });
+
+      space.addEventListener('keydown', (e) => {
+        if (e.keyCode === 13) {
+          e.preventDefault();
+          const index = parseInt(e.target.dataset.index, 10);
+          todoList[index].description = space.innerText;
+          localStorage.setItem('todos', JSON.stringify(todoList));
+          space.blur();
+        }
       });
     });
   });
